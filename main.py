d7a301c74ee23f02b7401b8fa08542834c93ebdc
Максим Астахов <mastahov112@gmail.com>
2024-02-03 12:13:35 +0300

    v3

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from PyQt5.QtCore import Qt
 from map_scale_utils import get_scale_params
 import requests
 import os
+import math
 
-coordinates = ['37.541676', '55.706857']
+coordinates = ['37.541776', '55.706857']
 base_url = 'https://static-maps.yandex.ru/1.x/'
 
 
@@ -29,6 +30,7 @@ class Map(QMainWindow):
         self.setGeometry(300, 300, 600, 450)
         self.setWindowTitle('Карта')
         self.z = 15
+        self.degr = 0.005
 
         self.label = QLabel(self)
         self.label.resize(600, 450)
@@ -45,9 +47,27 @@ class Map(QMainWindow):
         if event.key() == Qt.Key_PageUp:
             if self.z != 21:
                 self.z += 1
-        if event.key() == Qt.Key_PageDown:
+                self.degr /= 2.5
+        elif event.key() == Qt.Key_PageDown:
             if self.z != 1:
                 self.z -= 1
+                self.degr *= 2.5
+        elif event.key() == Qt.Key_Left:
+            cord = float(coordinates[0])
+            cord -= self.degr
+            coordinates[0] = str(cord)
+        elif event.key() == Qt.Key_Right:
+            cord = float(coordinates[0])
+            cord += self.degr
+            coordinates[0] = str(cord)
+        elif event.key() == Qt.Key_Up:
+            cord = float(coordinates[1])
+            cord += self.degr
+            coordinates[1] = str(cord)
+        elif event.key() == Qt.Key_Down:
+            cord = float(coordinates[1])
+            cord -= self.degr
+            coordinates[1] = str(cord)
         self.update()
 
 
